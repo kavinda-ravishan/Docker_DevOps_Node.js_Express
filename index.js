6deb2047cbea814e6f721fed6f3ca7cda9baf5c9
kavinda-ravishan <kvnravishan@outlook.com>
2021-05-01 17:19:07 +0530

    dumy express app

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h1>Web Server</h1>");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
